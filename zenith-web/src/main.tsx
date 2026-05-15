ff09a0babc77d4208fd2d6e63b5dabb445c9611c
Areg Kostanyan <areg.kostanyan@gmail.com>
2026-05-15 14:48:16 -0400

    chore: remove dark theme

--- zenith-web/src/main.tsx
+++ zenith-web/src/main.tsx
@@ -3,15 +3,12 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./App.tsx";
-import { ThemeProvider } from "@/components/theme-provider.tsx";
 import { ZenithEmulatorProvider } from "@/components/zenith-emulator-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <ZenithEmulatorProvider>
-        <App />
-      </ZenithEmulatorProvider>
-    </ThemeProvider>
+    <ZenithEmulatorProvider>
+      <App />
+    </ZenithEmulatorProvider>
   </StrictMode>
 );
