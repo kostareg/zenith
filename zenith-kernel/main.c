70c002f3bd5a1f053e8bbf859801e480b90fd735
Areg Kostanyan <areg.kostanyan@gmail.com>
2026-06-17 21:57:34 -0400

    feat(libc): scanf

--- zenith-kernel/main.c
+++ zenith-kernel/main.c
@@ -3,32 +3,15 @@
 #include <string.h>
 #include <math.h>
 
-#define KEYBOARD_STATUS_CONTROL 0xFFFFFFFFFFA00000ULL
-#define KEYBOARD_KEY_EVENT 0xFFFFFFFFFFA00004ULL
-#define KEYBOARD_FIFO_COUNT 0xFFFFFFFFFFA00008ULL
-
 int main() {
     enable_framebuffer();
-
-    uint32_t *control = KEYBOARD_STATUS_CONTROL;
-    uint32_t *key_event = KEYBOARD_KEY_EVENT;
-    uint32_t *fifo_count = KEYBOARD_FIFO_COUNT;
-
-    *control = 0x8;
+    enable_keyboard();
 
     while (1) {
-        uint32_t status = *control;
-
-        if (status & 0x1) {
-            uint32_t event = *key_event;
-
-            int values[4];
-            values[0] = event & 0xFFFF;
-            values[1] = (event >> 16) & 1;
-            values[2] = (event >> 17) & 1;
-            values[3] = *fifo_count;
-
-            printf_ints("k %d p %d r %d R %d\n", values);
+        char line[100] = {};
+        scanf(line);
+        if (strcmp(line, "run-command")) {
+            printf("yay\n");
         }
     }
 
--- zenith-libc/stdio.h
+++ zenith-libc/stdio.h
@@ -435,3 +435,106 @@ void printf_ints(char* word, int* xs) {
 //         }
 //     }
 // }
+
+char hid_to_ascii(uint32_t key_code) {
+    switch (key_code) {
+    case 0x04: return 'a';
+    case 0x05: return 'b';
+    case 0x06: return 'c';
+    case 0x07: return 'd';
+    case 0x08: return 'e';
+    case 0x09: return 'f';
+    case 0x0A: return 'g';
+    case 0x0B: return 'h';
+    case 0x0C: return 'i';
+    case 0x0D: return 'j';
+    case 0x0E: return 'k';
+    case 0x0F: return 'l';
+    case 0x10: return 'm';
+    case 0x11: return 'n';
+    case 0x12: return 'o';
+    case 0x13: return 'p';
+    case 0x14: return 'q';
+    case 0x15: return 'r';
+    case 0x16: return 's';
+    case 0x17: return 't';
+    case 0x18: return 'u';
+    case 0x19: return 'v';
+    case 0x1A: return 'w';
+    case 0x1B: return 'x';
+    case 0x1C: return 'y';
+    case 0x1D: return 'z';
+
+    case 0x1E: return '1';
+    case 0x1F: return '2';
+    case 0x20: return '3';
+    case 0x21: return '4';
+    case 0x22: return '5';
+    case 0x23: return '6';
+    case 0x24: return '7';
+    case 0x25: return '8';
+    case 0x26: return '9';
+    case 0x27: return '0';
+
+    case 0x28: return '\n';
+    case 0x2B: return '\t';
+    case 0x2C: return ' ';
+    case 0x2D: return '-';
+    case 0x2E: return '=';
+    case 0x2F: return '[';
+    case 0x30: return ']';
+    case 0x31: return '\\';
+    case 0x33: return ';';
+    case 0x34: return '\'';
+    case 0x35: return '`';
+    case 0x36: return ',';
+    case 0x37: return '.';
+    case 0x38: return '/';
+    }
+
+    return 0;
+}
+
+#define KEYBOARD_STATUS_CONTROL 0xFFFFFFFFFFA00000ULL
+#define KEYBOARD_KEY_EVENT 0xFFFFFFFFFFA00004ULL
+#define KEYBOARD_FIFO_COUNT 0xFFFFFFFFFFA00008ULL
+
+void enable_keyboard() {
+    uint32_t *control = KEYBOARD_STATUS_CONTROL;
+
+    *control = 0x8;
+}
+
+/// @brief C-style scanf function to poll from the keyboard.
+/// @param c reference to character buffer
+///
+/// Defaults to receiving characters, does not support other types. Note: the buffer should be large enough to store a
+/// line. Otherwise is undefined behaviour.
+void scanf(char* c) {
+    uint32_t *control = KEYBOARD_STATUS_CONTROL;
+    uint32_t *key_event = KEYBOARD_KEY_EVENT;
+    uint16_t i = 0;
+
+    while (1) {
+        uint32_t status = *control;
+
+        if (status & 0x1) {
+            uint32_t event = *key_event;
+            uint32_t key_code = event & 0xFFFF;
+            uint32_t pressed = (event >> 16) & 1;
+
+            if (key_code == 40) {
+                c[i] = 0;
+                return;
+            }
+
+            if (pressed) {
+                char ch = hid_to_ascii(key_code);
+                if (ch) {
+                    c[i] = ch;
+                    ++i;
+                }
+            }
+        }
+    }
+}
